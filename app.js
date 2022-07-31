eff863e4d448c1782e066c60d9da2d0317247bf7
keyeking <keyeking@126.com>
2022-07-31 23:11:44 +0800

    refactor: 项目搭建

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app=express()
+app.listen(3000, () => {
+    console.log('====================================');
+    console.log('服务器已启http:127.0.0.1:3000');
+    console.log('====================================');
+})
